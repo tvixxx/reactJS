b184d753b8fd2645f02e8bc20b3c0a81a61d39e6
shka <shka@htc-cs.ru>
2016-08-04 17:29:01 +0400

    clear project

--- src/localStore.js
+++
@@ -1,6 +0,0 @@
-export const get = () => JSON.parse(localStorage.getItem('state')) || undefined;
-export const set = (state, props) => {
-  let toSave = {};
-  props.forEach(p => toSave[p] = state[p]);
-  localStorage.setItem('state', JSON.stringify(toSave));
-};
